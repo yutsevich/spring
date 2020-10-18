70417b173c19b0104bc98291b996b2c50337a728
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-10-18 16:41:53 +0300

    implemented UserDao, UserService, added User model^ configured AppConfig

---
+++ src/main/java/org/example/dao/impl/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package org.example.dao.impl;
+
+import java.util.List;
+import org.example.dao.UserDao;
+import org.example.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not insert user " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM User ", User.class).getResultList();
+        }
+    }
+}
